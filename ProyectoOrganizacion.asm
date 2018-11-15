fc49c1b65ed54b495a2765d8e2ed00fdf32fee1b
Manuel Lecaro <mnlecaro@gmail.com>
2018-11-15 10:51:28 -0500

    agregando parte de toma de exponente

--- ProyectoOrganizacion.asm
+++ ProyectoOrganizacion.asm
@@ -16,8 +16,31 @@
 		li $v0,6
 		syscall
 		
+		#extraer el valor del simbolo si es positvo 0 y si es negativo 1 --0x80000000--  sirve como máscara
+		c.lt.d $f4, $zero			# CC = $f0 < $zero
+		bc1t mostrar_signo     			# Branch if $f0 < $zero
+		
+		#addi $t2, $zero, 0x80000000
+		#and $a0, $t1, $t2
+		#srl  $a0, $a0, 31
+		
+		#mostrando mensajes de resultados
+		li $v0, 4
+		la $a0, fin
+		syscall
+		
+		#mostrando mensaje de resultado para formato de 32 bits
+		li $v0, 4
+		la $a0, bits32
+		syscall 
+		
+		#mostrando el bit de signo
+		li $v0, 34
+		add.d $v0, $zero, 1
+		syscall
+		
 		#Mostrar el valor
 		li $v0,2
-		add.s $f12, $f0, $f4
+		add.d $f12, $f0, $f4
 		syscall
 		
