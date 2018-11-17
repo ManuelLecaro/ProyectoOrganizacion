49522c363895abc6dc4ae6e1e80a72b27cb5a2c6
Daniel Arroyo <darroyo@espol.edu.ec>
2018-11-17 12:43:43 -0500

    corregidos errores de compilacion, hay un problema con el registro en memoria.

--- ProyectoOrganizacion.c
+++ ProyectoOrganizacion.c
@@ -1,15 +1,15 @@
-#include<stdio>
+#include<stdio.h>
 #include <math.h>
 #include<stdio.h> 
 
-int aBinaro(int n, int i) {
+void aBinaro(int n, int i) {
     int k;
     for (i--; i >= 0; i--) {
-    k = n >> i;
-  	if (k & 1)
-      	printf("1");
-  	else
-     	printf("0");
+	    k = n >> i;
+	  	if (k & 1)
+	      	printf("1");
+	  	else
+	     	printf("0");
     }
 }
 
@@ -25,7 +25,7 @@ typedef union {
 typedef union {
     double num_original;
   	struct {
-        unsigned int fraccion : 52;
+        unsigned int fraccion;
         unsigned int exponente : 11;
         unsigned int signo : 1;
     } campo;
@@ -36,14 +36,14 @@ int main() {
    	doubleieee d;
     printf("Ingrese un numero de punto flotante: ");
 	scanf("%f%lf",&f.num_original, &d.num_original);
-	printf("")
+	printf("\n");
 	printf("Numero en formato simple: %d ",f.campo.signo);
 	aBinaro(f.campo.exponente, 8);
   	printf(" ");
   	aBinaro(f.campo.fraccion, 23);
    	printf("\n");
 
-   	printf("Numero en formato doble: \n",d.campo.signo);
+   	printf("Numero en formato doble: %d",d.campo.signo);
    	aBinaro(d.campo.exponente, 11);
   	printf(" ");
   	aBinaro(d.campo.fraccion, 52);
