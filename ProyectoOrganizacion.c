f95df6d780b9a327aedb91e88fa6a85142d84ac9
Daniel Arroyo <darroyo@espol.edu.ec>
2018-11-17 19:49:05 -0500

    Subido documentacion en Word. Modificado archivo en C con documentacion.

--- ProyectoOrganizacion.c
+++ ProyectoOrganizacion.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdio.h> 
 
-void aBinaro(int n, int i) {
-    int k;
+// Funcion que imprime el numero num en binario (solo i bits)
+void aBinaro(int num, int i) {
+    int bit;
     for (i--; i >= 0; i--) {
-	    k = n >> i;
-	  	if (k & 1)
+	    bit = num >> i;		//
+	  	if (bit & 1)
 	      	printf("1");
 	  	else
 	     	printf("0");
     }
 }
 
+// Estructura que almacena en una region de memoria el numero (32 bits) a convertir,
+// especificando el numero de bits por parte
 typedef union {
     float num_original;
   	struct {
@@ -20,39 +21,40 @@ typedef union {
         unsigned int exponente : 8;
         unsigned int signo : 1;
     } campo;
-} floatieee;
+} floatieee;		// sizeof(floatieee) devuelve 4 (bytes)
 
+// Estructura que almacena en una region de memoria el numero (64 bits) a convertir,
+// especificando el numero de bits por parte
 typedef union {
     double num_original;
   	struct {
         unsigned int fraccion: 32;
-        unsigned : 0;
-        unsigned int fraccion2: 20;
+        unsigned : 0;	// marca el inicio de un nuevo registro de 32 bits
+        unsigned int fraccion2: 20;	// parte faltante para completar los 52 bits de fraccion
         unsigned int exponente : 11;
         unsigned int signo : 1;
     } campo;
-} doubleieee;
+} doubleieee;		// sizeof(doubleieee) devuelve 8 (bytes)
 
 int main() {
-	printf("%d", sizeof(floatieee));
-	printf("%d", sizeof(doubleieee));
-	printf("%d", sizeof(double));
 
-   	floatieee f;
+   	floatieee f;	// numero de 32 bits a convertir
     printf("Ingrese un numero de punto flotante: ");
 	scanf("%f",&f.num_original);
-	printf("\n");
-	printf("Numero en formato simple: %d ",f.campo.signo);
+
+	// impresion del numero por partes
+	printf("\nNumero en formato simple: %d ",f.campo.signo);
 	aBinaro(f.campo.exponente, 8);
   	printf(" ");
   	aBinaro(f.campo.fraccion, 23);
    	printf("\n");
 
-   	doubleieee d;
+   	doubleieee d;	// numero de 64 bits a convertir
    	printf("Ingrese de nuevo el numero: ");
 	scanf("%lf", &d.num_original);
-	printf("\n");
-   	printf("Numero en formato doble: %d ",d.campo.signo);
+
+	// impresion del numero por partes
+   	printf("\nNumero en formato doble: %d ",d.campo.signo);
    	aBinaro(d.campo.exponente, 11);
   	printf(" ");
   	aBinaro(d.campo.fraccion2, 20);
