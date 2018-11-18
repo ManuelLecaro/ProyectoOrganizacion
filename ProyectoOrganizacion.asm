11dd61e480c470aa397e9831860e207d4ead5a32
Manuel Lecaro <mnlecaro@gmail.com>
2018-11-17 23:17:51 -0500

    Terminado formato IEEE-754 doble

--- ProyectoOrganizacion.asm
+++ ProyectoOrganizacion.asm
@@ -31,7 +31,9 @@ main:
     
     
     mfc1   $s0,$f0		#mueve el valor de $so a $f0
-
+	
+    add    $s7,$zero,$zero	#$s7 determinada como variable
+    
     #simple
     la      $a0,simple		#muestra el mensaje del resultado en formato simple
     li      $a1,32		#carga 32 en a1
@@ -39,13 +41,18 @@ main:
     
     #doble
     la      $a0,double		#mostrando el mensaje de resultado en formato doble
-    li      $a1,64		
+    li      $a1,32		
     mfc1.d  $s0,$f0		#mueve el valor de simple a doble 
     jal     printdoub		#moviendose a printdoub
+    
+    #doble
+    la      $a0,double		#mostrando el mensaje de resultado en formato doble
+    li      $a1,32		
+    jal     printdoub_second		#moviendose a printdoub
 
     #hexadecimal
     la      $a0,hex		#mostrando el mensaje de resultado en hexadecimal
-    li      $a1,32		
+    li      $a1,16		
     jal     printhex		#moviendose a printhex
     
     # separando bits en posicion 12,13,14,15
@@ -69,6 +76,14 @@ printsmpl:
 printdoub:
     li      $a2,1                   # base de numero a imprimir 1, porque es binario
     j       prtanydouble
+
+# imprimiendo en formato doble
+#   $a0: donde se guarda el string de salida
+#   $a1: numero de bits que se toman para imprimir
+printdoub_second:
+    li      $a2,1                   # base de numero a imprimir 1, porque es binario
+    j       second_part
+    
     
 # imprimiendo en formato hexadecimal
 #   $a0: donde se guarda el string de salida
@@ -101,13 +116,13 @@ prtany:
 
 prtanyhex:
     
-    cvt.d.s $f2,$f0
+    cvt.w.s $f2,$f0
 
     li      $v0,4		    #mostrando el valor en $a0
     syscall
 
-    mfc1    $a0,$f2
-    li      $v0,35
+    mfc1.d    $a0,$f2
+    li      $v0,34
     syscall  
     j       salto
     
@@ -122,8 +137,9 @@ prtanydouble:
     sb      $zero,0($t6)            # guardando string
 
     #move    $t2,$s0	                   # guardando el numero
-    la	    $t2, space		   #guardando un espacio de 64bits en $t2
-    ldc1    $f2, 8($t2) 
+    cvt.d.s $f2,$f0		   #guardando un espacio de 64bits en $t2
+    mfc1.d  $s3,$f2
+    move    $t2,$s4
     
     
 prt_loop_double:
@@ -136,7 +152,35 @@ prt_loop_double:
     srlv    $t2,$t2,$a2             # moviendose la cantidad de numeros que en $a2 de bits
     sub     $a1,$a1,$a2             # reduciendo la cantidad de bits que quedan por mostrar
     bgtz    $a1,prt_loop_double   # si quedan valores, continuar
+    addi    $s7,$zero,1
     j 	    printing
+
+#referente a la segunda parte de los 32bits con los que se imprime el formato IEEE-754 doble
+second_part:
+    li      $t7,1
+    sllv    $t7,$t7,$a2             # haciendo mascara de bits con un shift logico segun la base del numero a mostrar
+    subu    $t7,$t7,1               # haciendo la mascara para cada digito
+
+    la      $t6,buffer              # seteando el fin del buffer
+    subu    $t6,$t6,1               # puntero al char final
+    sb      $zero,0($t6)            # guardando string
+
+    mfc1.d  $s5,$f2		    #moviendo $f2 a $s5, se guardan solo los primeros 32 bits del valor
+    move    $t3,$s5		    #guardando el valor
+    
+prt_loop_double_rest:
+    and     $t0,$t3,$t7             # separando el digito
+    lb      $t0,hexDigit($t0)       # tomando el valor ASCII
+
+    subu    $t6,$t6,1               # moviendose hacia la derecha un bit
+    sb      $t0,0($t6)              # guardando en el buffer
+
+    srlv    $t3,$t3,$a2             # moviendose la cantidad de numeros que en $a2 de bits
+    sub     $a1,$a1,$a2             # reduciendo la cantidad de bits que quedan por mostrar
+    bgtz    $a1,prt_loop_double_rest   # si quedan valores, continuar
+    move    $s7, $zero
+    j 	    printing_second_part
+	
    
 print_loop:
     and     $t0,$t5,$t7             # separando el digito
@@ -153,11 +197,17 @@ printing:
     li      $v0,4		    #mostrando el valor en $a0
     syscall
 
+#caso para mostrar los 32 bits restantes para el formato IEEE-754 doble
+printing_second_part:
     move    $a0,$t6                 #moviendo los valores en el buffer a $a0
     syscall
-
+   
+    beq     $s7,$zero,salto	    #para evitar el salto de linea cuando se imprimen los 64 bits
+    j no_saltes
+    
 salto:
     la      $a0,nl		    #salto de linea
     syscall
-
+    
+no_saltes:
     jr      $ra     		    #retornando
