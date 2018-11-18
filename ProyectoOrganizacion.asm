b51f4a7d445eb0c7a9e3038c0e2eda1192020dd9
Manuel Lecaro <mnlecaro@gmail.com>
2018-11-17 22:24:50 -0500

    Cambios en archivo .asm

--- ProyectoOrganizacion.asm
+++ ProyectoOrganizacion.asm
@@ -1,177 +1,163 @@
-# Read integer A from user and store it into a register
-# Display the integer in binary
-# Display the integer in Hex
+# Lee el valor ingresado por el usuario y lo guarda en un registro
+# Muestra el valor en formato IEEE-754 simple
+# Muestra el valor en formato IEEE-754 doble
+# Muestra el valor en formato Hexadecimal
 # set Register $a0 to contain only bits 12,13,14,15 of $a0
-# Display the integer in binary contained in $a0
-# Display the integer in hex contained in $a0
 
-    .data
-userInput:  .asciiz     "Please enter your integer: "
-binaryInput:    .asciiz "Here is the input in binary: "
-doubleInput:   .asciiz "Here is the output in IEEE-757 double: "
-nl:         .asciiz     "\n"
-hexInput:   .asciiz     "Here is the input in hexadecimal: "
-binaryOutput:   .asciiz "Here is the output in IEEE-754 simple: "
-doubleOutput:   .asciiz "Here is the output in IEEE-757 double: "
-hexOutput:  .asciiz     "Here is the output in hexadecimal: "
-hexDigit:   .asciiz     "0123456789ABCDEF"
-obuf:       .space      100
+.data
+space_u: .space		32
+space:.space		64
+user: .asciiz     	"Ingrese el número que quiera transformar al formato IEE-754: "
+simple:    .asciiz 	"Este es su número en formato IEEE-754 simple: "
+double:    .asciiz 	"Este es su número en formato IEEE-754 double: "
+nl:         .asciiz     	"\n"
+hex:   	   .asciiz     	"Este es su número en hexadecimal: "
+hexDigit:   .asciiz     	"0123456789ABCDEF"
+obuf:       .space      	100
 zerodouble: .double	0.0
-obufe:
+zerosimple: .float	0.0
+buffer:
 
-    .text
-    .globl  main
+.text
+.globl  main
 main:
-    # ask end-user to submit an integer value
-    li      $v0,4
-    la      $a0,userInput
+    li      $v0,4		#muestra el mensaje para pedir valor al usuario
+    la      $a0,user
     syscall
-
-    # read user-input
-    li      $v0,6
+    
+   
+    li      $v0,6		#guarda el valor de un flotante en el registro $f0
     syscall
-    mfc1    $s0,$f0
     
-    #move user-input to double
-    lwc1  $f6, zerodouble
-    add.d $f2, $f0, $f6
-
-    # output original in binary
-    la      $a0,binaryInput
-    li      $a1,32
-    jal     prtbin
     
-    # output original in double
-    la      $a0,doubleInput
-    li      $a1,64
-    mfc1.d  $s0,$f0
-    jal     prtdoub
+    mfc1   $s0,$f0		#mueve el valor de $so a $f0
 
-    # output original in hex
-    la      $a0,hexInput
-    li      $a1,32
-    jal     prthex
+    #simple
+    la      $a0,simple		#muestra el mensaje del resultado en formato simple
+    li      $a1,32		#carga 32 en a1
+    jal     printsmpl		#moviendose a prtbin
+    
+    #doble
+    la      $a0,double		#mostrando el mensaje de resultado en formato doble
+    li      $a1,64		
+    mfc1.d  $s0,$f0		#mueve el valor de simple a doble 
+    jal     printdoub		#moviendose a printdoub
 
-    # isolate bits 12,13,14,15
+    #hexadecimal
+    la      $a0,hex		#mostrando el mensaje de resultado en hexadecimal
+    li      $a1,32		
+    jal     printhex		#moviendose a printhex
+    
+    # separando bits en posicion 12,13,14,15
     srl     $s0,$s0,12
     andi    $s0,$s0,0x0F
 
-    # output isolated in binary
-    la      $a0,binaryOutput
-    li      $a1,4
-    jal     prtbin
-    
-    # output isolated in double
-    la      $a0,doubleOutput
-    li      $a1,4
-    jal     prtdoub
-
-    # output isolated in hex
-    la      $a0,hexOutput
-    li      $a1,4
-    jal     prthex
-
-    # exit the program
-    li      $v0,10
+    li      $v0,10		#Terminando
     syscall
 
-# prtbin -- print in binary
-#
-# arguments:
-#   a0 -- output string
-#   a1 -- number of bits to output
-prtbin:
-    li      $a2,1                   # bit width of number base digit
+
+#imprimiendo en formato simple
+#   $a0: donde se guarda el string de salida
+#   $a1: numero de bits que se toman para imprimir
+printsmpl:
+    li      $a2,1                   # base del numero a imprimir
     j       prtany
 
-# prtbin -- print in binary
-#
-# arguments:
-#   a0 -- output string
-#   a1 -- number of bits to output
-prtdoub:
-    li      $a2,1                   # bit width of number base digit
+# imprimiendo en formato doble
+#   $a0: donde se guarda el string de salida
+#   $a1: numero de bits que se toman para imprimir
+printdoub:
+    li      $a2,1                   # base de numero a imprimir 1, porque es binario
     j       prtanydouble
     
-# prthex -- print in binary
-#
-# arguments:
-#   a0 -- output string
-#   a1 -- number of bits to output
-prthex:
-    li      $a2,4                   # bit width of number base digit
-    j       prtany
+# imprimiendo en formato hexadecimal
+#   $a0: donde se guarda el string de salida
+#   $a1: numero de bits que se toman para imprimir
+printhex:
+    li      $a2,4                   # base 4, para imprimir en hexadecimal
+    j       prtanyhex
 
-# prtany -- print in given number base
-#
-# arguments:
-#   a0 -- output string
-#   a1 -- number of bits to output
-#   a2 -- bit width of number base digit
-#   s0 -- number to print
-#
-# registers:
-#   t0 -- current digit value
-#   t5 -- current remaining number value
-#   t6 -- output pointer
-#   t7 -- mask for digit
+# imprimiendo segun la base del numero
+#   a0:  donde se guarda el string
+#   a1: cantidad de bits que se muestran
+#   a2: base del numero
+#   s0: numero a imprimir
+# registros temporales
+#   t0: digito actual
+#   t5: valor restante del digito
+#   t6: puntero de salida
+#   t7: mascar de bits
 prtany:
     li      $t7,1
-    sllv    $t7,$t7,$a2             # get mask + 1
-    subu    $t7,$t7,1               # get mask for digit
+    sllv    $t7,$t7,$a2             # haciendo mascara de bits con un shift logico segun la base del numero a mostrar
+    subu    $t7,$t7,1               # haciendo la mascara para cada digito
 
-    la      $t6,obufe               # point one past end of buffer
-    subu    $t6,$t6,1               # point to last char in buffer
-    sb      $zero,0($t6)            # store string EOS
+    la      $t6,buffer              # seteando el fin del buffer
+    subu    $t6,$t6,1               # puntero al char final
+    sb      $zero,0($t6)            # guardando string
 
-    move    $t5,$s0                 # get number
-    j	    prtany_loop
+    move    $t5,$s0                 # guardando el numero
+    j	    print_loop		    #moviendose a prtany_loop
+
+prtanyhex:
+    
+    cvt.d.s $f2,$f0
+
+    li      $v0,4		    #mostrando el valor en $a0
+    syscall
+
+    mfc1    $a0,$f2
+    li      $v0,35
+    syscall  
+    j       salto
+    
 
 prtanydouble:
     li      $t7,1
-    sllv    $t7,$t7,$a2             # get mask + 1
-    subu    $t7,$t7,1               # get mask for digit
+    sllv    $t7,$t7,$a2             # haciendo mascara de bits con un shift logico segun la base del numero a mostrar
+    subu    $t7,$t7,1               # haciendo la mascara para cada digito
 
-    la      $t6,obufe               # point one past end of buffer
-    subu    $t6,$t6,1               # point to last char in buffer
-    sb      $zero,0($t6)            # store string EOS
+    la      $t6,buffer              # seteando el fin del buffer
+    subu    $t6,$t6,1               # puntero al char final
+    sb      $zero,0($t6)            # guardando string
 
-    mov.d    $f8,$f2                 # get number
+    #move    $t2,$s0	                   # guardando el numero
+    la	    $t2, space		   #guardando un espacio de 64bits en $t2
+    ldc1    $f2, 8($t2) 
     
-prtany_loop_double:
-    and     $t0,$t5,$t7             # isolate digit
-    lb      $t2,hexDigit($t0)       # get ascii digit
+    
+prt_loop_double:
+    and     $t0,$t2,$t7             # separando el digito
+    lb      $t0,hexDigit($t0)       # tomando el valor ASCII
 
-    subu    $t6,$t6,1               # move output pointer one left
-    sb      $t0,0($t6)              # store into output buffer
+    subu    $t6,$t6,1               # moviendose hacia la derecha un bit
+    sb      $t0,0($t6)              # guardando en el buffer
 
-    srlv    $t5,$t5,$a2             # slide next number digit into lower bits
-    sub     $a1,$a1,$a2             # bump down remaining bit count
-    bgtz    $a1,prtany_loop         # more to do? if yes, loop
+    srlv    $t2,$t2,$a2             # moviendose la cantidad de numeros que en $a2 de bits
+    sub     $a1,$a1,$a2             # reduciendo la cantidad de bits que quedan por mostrar
+    bgtz    $a1,prt_loop_double   # si quedan valores, continuar
     j 	    printing
+   
+print_loop:
+    and     $t0,$t5,$t7             # separando el digito
+    lb      $t0,hexDigit($t0)       # tomando el valor ASCII
 
-prtany_loop:
-    and     $t0,$t5,$t7             # isolate digit
-    lb      $t0,hexDigit($t0)       # get ascii digit
+    subu    $t6,$t6,1               # moviendose hacia la derecha un bit
+    sb      $t0,0($t6)              # guardando en el buffer
 
-    subu    $t6,$t6,1               # move output pointer one left
-    sb      $t0,0($t6)              # store into output buffer
-
-    srlv    $t5,$t5,$a2             # slide next number digit into lower bits
-    sub     $a1,$a1,$a2             # bump down remaining bit count
-    bgtz    $a1,prtany_loop         # more to do? if yes, loop
+    srlv    $t5,$t5,$a2             # moviendose la cantidad de numeros que en $a2 de bits
+    sub     $a1,$a1,$a2             # reduciendo la cantidad de bits que quedan por mostrar
+    bgtz    $a1,print_loop         # si quedan valores, continuar
 
 printing:
-    # output string
-    li      $v0,4
+    li      $v0,4		    #mostrando el valor en $a0
     syscall
 
-    # output the number
-    move    $a0,$t6                 # point to ascii digit string start
+    move    $a0,$t6                 #moviendo los valores en el buffer a $a0
     syscall
 
-    # output newline
-    la      $a0,nl
+salto:
+    la      $a0,nl		    #salto de linea
     syscall
 
-    jr      $ra     
+    jr      $ra     		    #retornando
